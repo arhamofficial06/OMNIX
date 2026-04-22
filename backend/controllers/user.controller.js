26ae1d660010fbe46040c923258b49e2dc75a39c
Muhammad Arham <arhamofficial28@gmail.com>
2026-04-22 14:32:43 +0500

    Backend data Integrate to frontend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,6 +15,8 @@ export const createUserController = async (req, res) => {
 
     const token = await user.generateJwt();
 
+    delete user._doc.password;
+
     res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error.message);
@@ -44,6 +46,8 @@ export const loginController = async (req, res) => {
     }
 
     const token = await user.generateJwt();
+
+    delete user._doc.password;
 
     res.status(200).send({ user, token });
   } catch (error) {
