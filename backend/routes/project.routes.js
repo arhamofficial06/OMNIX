41894f693e65b188c2d396cff1acceb8e5db1761
Muhammad Arham <arhamofficial28@gmail.com>
2026-04-24 18:03:48 +0500

    feat: add get project by ID endpoint

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -27,6 +27,10 @@ router.put(
   projectController.addUserToProject,
 );
 
-
+router.get(
+  "/get-project/:projectId",
+  authMiddleware.authUser,
+  projectController.getProjectById,
+);
 
 export default router;
--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -91,3 +91,21 @@ export const addUsersToProject = async ({ projectId, users, userId }) => {
 
   return updatedProject;
 };
+
+export const getProjectById = async ({ projectId }) => {
+  if (!projectId) {
+    throw new Error("ProjectId is required");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    throw new Error("Invalid projectId");
+  }
+
+  const project = await projectModel
+    .findOne({
+      _id: projectId,
+    })
+    .populate("users");
+
+  return project;
+};
